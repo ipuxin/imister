5ab719dd715bf3e49aa9d782a156a2d42b85b7d2
ipuxin <ipuxin@qq.com>
2016-10-23 21:04:22 +0800

    完成类别的删除,类别有bug,这种写法不太好.

--- backend/controllers/CategoryController.php
+++ backend/controllers/CategoryController.php
@@ -31,4 +31,33 @@ class CategoryController extends AdminController
         }
         return $this->render('add', ['model' => $model]);
     }
+
+    public function actionEdit($id)
+    {
+        $id = (int)$id;
+        $model = Category::findOne($id);
+        if ($model) {
+            if (Yii::$app->request->isPost && $model->load(Yii::$app->request->post()) && $model->save()) {
+                Yii::$app->session->setFlash('success', '编辑文章分类成功');
+                return $this->redirect(['index']);
+            }
+            return $this->render('edit', ['model' => $model]);
+        }
+        return $this->redirect(['index']);
+    }
+
+    /**
+     * @return \yii\web\Response
+     * 单个,多个删除
+     */
+    public function actionDelete()
+    {
+        $selected = Yii::$app->request->post('selected');
+        if (Category::deleteIn($selected)) {
+            Yii::$app->session->setFlash('success', '删除文章分类成功');
+        } else {
+            Yii::$app->session->setFlash('error', '删除文章分类失败');
+        }
+        return $this->redirect(['index']);
+    }
 }
--- common/models/Category.php
+++ common/models/Category.php
@@ -20,18 +20,35 @@ class Category extends ActiveRecord
     public function rules()
     {
         return [
-            ['pid', 'integer', 'min' => 0 , 'tooSmall' => '不能小于0的整数', 'message' => '不能小于0的整数'],
+            ['pid', 'integer', 'min' => 0, 'tooSmall' => '不能小于0的整数', 'message' => '不能小于0的整数'],
             ['name', 'required', 'message' => '名称不能为空'],
             ['name', 'string', 'max' => 30, 'tooLong' => '名称长度不能大于30位'],
             ['sort_order', 'integer', 'min' => 0, 'tooSmall' => '不能小于0的整数', 'message' => '不能小于0的整数'],
-            ['status', 'in', 'range' => [0, 1], 'message'=>'非法操作'],
+            ['status', 'in', 'range' => [0, 1], 'message' => '非法操作'],
+            ['pid', 'checkPid'],
         ];
+    }
+
+    /**
+     * @param $attribute
+     * @param $params
+     * 验证修改时:
+     * 如果类别有子类,就不能修改;
+     * 如果设置自身为父类,也不能保存修改
+     */
+    public function checkPid($attribute, $params)
+    {
+        if (self::find()->where(['pid' => $this->id])->count() > 0) {
+            $this->addError($attribute, '该类下有子类，请先移除');
+        } elseif ($this->id == $this->$attribute) {
+            $this->addError($attribute, '无法成为自身的子类');
+        }
     }
 
     public function beforeSave($insert)
     {
         if (parent::beforeSave($insert)) {
-            if($this->isNewRecord) {
+            if ($this->isNewRecord) {
                 $this->date = time();
             }
             return true;
@@ -53,4 +70,14 @@ class Category extends ActiveRecord
         return ArrayHelper::merge([0 => '父类'], ArrayHelper::map($data, 'id', 'name'));
     }
 
+    /**
+     * @param $selected
+     * @return int
+     * 删除
+     */
+    public static function deleteIn($selected)
+    {
+        $selected = array_map('intval', $selected);
+        return self::deleteAll(['id' => $selected]);
+    }
 }
